86edcab70bc0bb4366d7dc20d6977207495423bd
xiaozhi001 <18638389375@163.com>
2021-01-19 16:41:03 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,27 @@ import VueRouter from 'vue-router'
 
 // 懒加载组件，通过路由添加映射关系
 const Login= () => import('views/Login')
+const Test= () => import('views/Test')
 
 // 1.安装插件
 Vue.use(VueRouter)
 
 const routes =[
+  // {
+  //   path: '',
+  //   redirect: '/login'
+  // },
+  // {
+  //   path: '/login',
+  //   component: Login
+  // },
   {
     path: '',
-    redirect: '/login'
+    redirect: '/test'
   },
   {
-    path: '/login',
-    component: Login
+    path: '/test',
+    component: Test
   }
 ]
 
---
+++ src/views/Test.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <el-container>
+        <el-header>
+            <button @click="collapseStatus">
+                <i class="el-icon-d-arrow-right">xiaozhi</i>
+            </button>
+        </el-header>
+        <el-container>
+            <el-aside
+                    width="auto"
+                    @mouseenter.native="collapseOpen"
+                    @mouseleave.native="collapseClose">
+                <el-menu :collapse="isCollapse">
+                    <el-menu-item index="1">
+                        <i class="el-icon-menu"></i>
+                        <span slot="title">测试</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
+            <el-main>
+            </el-main>
+        </el-container>
+    </el-container>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data(){
+    return {
+      collapseBtnClick: false,
+      isCollapse: true,
+    }
+  },
+  methods: {
+    collapseStatus() {
+      this.collapseBtnClick = this.isCollapse;
+      this.isCollapse = !this.isCollapse;
+    },
+    collapseOpen() {
+      if (this.collapseBtnClick) return;
+      this.isCollapse = false;
+    },
+    collapseClose() {
+      if (this.collapseBtnClick) return;
+      this.isCollapse = true;
+    }
+
+  }
+  
+}
+</script>
+<style scoped>
+</style>
